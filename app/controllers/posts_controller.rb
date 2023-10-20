c3a574edda0751db0010674ea0b4d6c6de8a43ec
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-10-20 10:18:32 +0300

    implmenting deleting on comments and posts for the controller and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+  before_action :set_current_user, only: %i[new create destroy]
+
   def index
-    # @user = User.find(params[:user_id])
     @user = User.includes(posts: :comments).find(params[:user_id])
     @posts = @user.posts.includes(:comments)
   end
 
   def show
     @user = User.find(params[:user_id])
-    # @post = @user.posts.find(params[:id])
     @post = @user.posts.includes(:comments).find(params[:id])
   end
 
@@ -27,5 +28,37 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @post = @user.posts.find(params[:id])
+
+    if @post
+      begin
+        @post.comments.destroy_all
+        @post.likes.destroy_all
+        if @post.destroy
+          flash.now[:success] = "Your post titled: '#{@post.title}' was successfully deleted"
+        else
+          flash.now[:error] = 'Oops! Cannot delete your post.'
+        end
+      rescue StandardError => e
+        flash.now[:error] = "An error occurred: #{e.message}"
+      end
+    else
+      flash.now[:error] = 'Post not found.'
+    end
+
+    redirect_to user_posts_path(@user)
+  end
+
+  private
+
+  def set_current_user
+    @user = current_user
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
   devise_for :users
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create destroy] do
-      resources :comments, only: %i[new create destroy]
-      resources :likes, only: %i[new create]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:index, :new, :create, :destroy]
+      resources :likes, only: [:new, :create]
     end
 end
 root 'users#index'
